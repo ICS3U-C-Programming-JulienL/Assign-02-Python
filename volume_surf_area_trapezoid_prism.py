12eb2e336ef47b9e8d84f0f6e525a4c19c6d27a3
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-15 17:10:23 +0000

    Finished volume_surf_area_trapezoid_prism.py

--- volume_surf_area_trapezoid_prism.py
+++ volume_surf_area_trapezoid_prism.py
@@ -8,13 +8,13 @@
 def main():
     # get all sides of the trapezoid and the units
     print("Calculating the Surface Area and Volume of a Trapezoidal Prism")
-    side_a = float(input("How long is side a (the left diagonal)?"))
-    side_b = float(input("How long is side b (the long base)?"))
-    side_c = float(input("How long is side c (the right diagonal)?"))
-    side_d = float(input("How long is side d (the short base)"))
-    length = float(input("What is the length of the prism?"))
-    height = float(input("What is the height of the prism?"))
-    units = str(input("What units would you like to be used (ex. cm)"))
+    side_a = float(input("How long is side a (the left diagonal): "))
+    side_b = float(input("How long is side b (the base on the bottom): "))
+    side_c = float(input("How long is side c (the right diagonal): "))
+    side_d = float(input("How long is side d (the base on the top): "))
+    length = float(input("What is the length of the prism: "))
+    height = float(input("What is the height of the prism: "))
+    units = str(input("What units would you like to be used (ex. cm): "))
 
     # calculate the surface area and volume
     surface_area = height * (side_b + side_d) + length * (
@@ -24,21 +24,21 @@ def main():
 
     # if any of the sides <= 0, then tell the user to enter a positive number.
     if side_a <= 0:
-        print("Please enter a positive number for all input options except the  units.")
+        print("Please enter a positive number for side a.")
     elif side_b <= 0:
-        print("Please enter a positive number for all input option except the  units.")
+        print("Please enter a positive number for side b.")
     elif side_c <= 0:
-        print("Please enter a positive number for all input options except the  units.")
+        print("Please enter a positive number for side c.")
     elif side_d <= 0:
-        print("Please enter a positive number for all input options except the  units.")
+        print("Please enter a positive number for side d.")
     elif length <= 0:
-        print("Please enter a positive number for all input options except the  units.")
+        print("Please enter a positive number for the length.")
     elif height <= 0:
-        print("Please enter a positive number for all input options except the  units.")
+        print("Please enter a positive number for the height.")
     else:
         # otherwise, display the surface area and volume
-        print("The surface area is {0} {1}^2".format(surface_area, units))
-        print("The volume {0} {1}^3".format(volume, units))
+        print("The surface area is {0:.2f} {1}^2.".format(surface_area, units))
+        print("The volume is {0:.2f} {1}^3.".format(volume, units))
 
 
 if __name__ == "__main__":
